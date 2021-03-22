f1537685314bbeee6c491457271f846456920039
sudhashrinaik <55624217+sudhashrinaik@users.noreply.github.com>
2021-03-22 18:30:26 +0530

    added sub function

--- SampleGitDemo/src/main/Calculator.java
+++ SampleGitDemo/src/main/Calculator.java
@@ -13,5 +13,9 @@ public class Calculator {
 	public static void add(int num1,int num2){
 	      System.out.println(num1+num2);
 	  }
+	
+	public static void sub(int num1,int num2){
+	      System.out.println(num1-num2);
+	  }
 
 }
