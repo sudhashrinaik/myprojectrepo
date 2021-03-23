e53ad9c1c3075ea07eed8a5cbd9d4353e705c542
sudhashri naik <sudhashrinaik@gmail.com>
2021-03-23 10:44:47 +0530

    added masterfun1 in master

--- SampleGitDemo/src/main/Calculator.java
+++ SampleGitDemo/src/main/Calculator.java
@@ -37,6 +37,10 @@ public class Calculator {
 	public static void cube(int num1){
 	      System.out.println(num1*num1*num1);
 	  }
+	
+	public static void masterfun1(int num1){
+	      System.out.println(num1*num1*num1);
+	  }
 
 
 }
