d0e8321d76726c7b766be9fa476c26809ec592df
sudhashri naik <sudhashrinaik@gmail.com>
2021-03-23 11:21:32 +0530

    Merge branch 'master' of https://github.com/sudhashrinaik/myprojectrepo.git
    Conflicts:
    	SampleGitDemo/src/main/Calculator.java

--- SampleGitDemo/src/main/Calculator.java
+++ SampleGitDemo/src/main/Calculator.java
@@ -47,6 +47,9 @@ public class Calculator {
 	public static void br1fun2(int num1){
 	      System.out.println(num1*num1*num1);
 	  }
+	public static void br1fun3(int num1){
+	      System.out.println(num1*num1*num1);
+	  }
 
 
 }
