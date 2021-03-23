01521906ed52b193dc655ba6e3d3faef6ff2d6aa
sudhashri naik <sudhashri naik@DESKTOP-UVUGCCB>
2021-03-23 09:49:49 +0530

    added div and mod function

--- SampleGitDemo/src/main/Calculator.java
+++ SampleGitDemo/src/main/Calculator.java
@@ -21,5 +21,13 @@ public class Calculator {
 	public static void prod(int num1,int num2){
 	      System.out.println(num1*num2);
 	  }
+	
+	public static void mod(int num1,int num2){
+	      System.out.println(num1%num2);
+	  }
+	public static void div(int num1,int num2){
+	      System.out.println(num1/num2);
+	  }
+
 
 }
